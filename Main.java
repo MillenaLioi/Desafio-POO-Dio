755a5efe3e7da7dff68a8a918c256b851c6ce333
Millenalioi <millena.lioi@gmail.com>
2022-04-03 23:47:38 -0300

    Desafio POO

---
+++ Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+    public static void main (String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descricao curso js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descricao mentoria java");
+        mentoria.setDate(LocalDate.now());
+
+        System.out.pintln(curso1);
+        System.out.pintln(curso2);
+        System.out.pintln(mentoria);
+
+        }
+
+    } 
